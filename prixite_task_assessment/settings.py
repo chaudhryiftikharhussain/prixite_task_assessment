5110d93468c3f60cc329901a9c1aed8d42d83a85
chaudhryiftikharhussain <chaudhryiftikharhussain@outlook.com>
2023-08-04 01:06:48 +0500

    testing flake8

--- prixite_task_assessment/settings.py
+++ prixite_task_assessment/settings.py
@@ -58,7 +58,8 @@ ROOT_URLCONF = 'prixite_task_assessment.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
